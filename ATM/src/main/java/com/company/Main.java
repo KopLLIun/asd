b04c5ddcbedeafc37ce07698acf5e0c847272768
Nikita Radevich <nikita.radevich@intexsoft.by>
2020-03-12 16:22:23 +0300

    asd

---
+++ ATM/src/main/java/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import javax.xml.bind.JAXBException;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws JAXBException, IOException {
+//        String path = args[0];
+//        ClientInformation client = (ClientInformation) ParserXML.getObject ( new File ( "src/main/resources/CashCard.xml" ), ClientInformation.class );
+//        System.out.println ( client );
+/*        UserInterface userInterface = new UserInterface();
+        userInterface.startProgram();*/
+        String s = DialogTypes.values()[0].toString();
+        System.out.println(s);
+    }
+}
+
